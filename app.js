f8d6f965f8f91ceff4cc2c444a90712d7dbf29ef
이지환 사원 데브옵스파트 (jeehwan_lee@biztechi.com <jeehwan_lee@cns.lgcns.com>
2023-08-29 13:27:00 +0900

    db 연결정보 설정

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const mysql = require("./db/db");
 
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
