4e612061f1079d2709f448ae581460fdf6d93462
jeehwan <tjtojan@naver.com>
2023-08-30 00:20:24 +0900

    글작성 페이지 개발

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mysql = require("./db/db");
 
 const postService = require("./services/post_service");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
@@ -26,11 +29,32 @@ app.get("/", (req, res) => {
 });
 
 app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트 게시판" });
+  res.render("write", { title: "글 작성 페이지", mode: "c" });
 });
 
+app.post("/write", (req, res) => {
+  const post = req.body;
+  console.log(post);
+
+  const sql = `INSERT INTO post (title, writer, password, content, hits) values('${post.title}', '${post.writer}', ${post.password}, '${post.content}', 0)`;
+
+  console.log(sql);
+  mysql.getConnection((err, connection) => {
+    connection.query(sql, (err, result, fields) => {
+      if (!err) {
+        res.redirect(`/detail/${result.insertId}`);
+      } else {
+        throw err;
+      }
+    });
+    connection.release();
+  });
+});
+
+app.get;
+
 app.get("/detail/:id", async (req, res) => {
-  const sql = `UPDATE post SET hits=hits+1 WHERE id = 1`;
+  const sql = `UPDATE post SET hits=hits+1 WHERE id = ${req.params.id}`;
 
   mysql.getConnection((err, connection) => {
     connection.query(sql, (err, result, fields) => {
