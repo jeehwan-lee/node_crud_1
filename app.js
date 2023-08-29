d56bcbfe3a4dc8164d5f6026f99c6bfd4f1a2401
이지환 사원 데브옵스파트 (jeehwan_lee@biztechi.com <jeehwan_lee@cns.lgcns.com>
2023-08-29 14:13:53 +0900

    mysql connection pool 연결

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
+  const sql = "SELECT * FROM post";
+
+  mysql.getConnection((err, connection) => {
+    connection.query(sql, (err, result, fields) => {
+      if (!err) {
+        console.log(result);
+        connection.release();
+      } else {
+        throw err;
+      }
+    });
+  });
   res.render("home", { title: "안녕하세요", message: "반갑습니다." });
 });
 
